4629b53fd83af94d8dca9238105cb3ca236d7489
Rushi Dave <rushidave7388@gmail.com>
2026-01-30 19:32:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("Hello World")
+
+console.log("I am Learning DevOps CI/CD..")
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log("Starting Tests....")
+
+setTimeout(() => console.log("Waiting for 5 Seconds..."), 5000)
+
+console.log("Tests Completed...");
